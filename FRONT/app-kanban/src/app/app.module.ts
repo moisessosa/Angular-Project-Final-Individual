144f3d713063c2c7a4093c3511ea8464f20a7c5d
moisessosa <rod20028@byui.com>
2021-11-16 02:50:13 -0300

    tenho que fazer swicth dentro do card

--- FRONT/app-kanban/src/app/app.module.ts
+++ FRONT/app-kanban/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
