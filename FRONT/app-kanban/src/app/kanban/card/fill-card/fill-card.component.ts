be0b79572ccfcf128708532c33142575fe4aecb8
moisessosa <rod20028@byui.com>
2021-11-28 03:06:48 -0300

    boton derecho pronto

--- FRONT/app-kanban/src/app/kanban/card/fill-card/fill-card.component.ts
+++ FRONT/app-kanban/src/app/kanban/card/fill-card/fill-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { APIService } from '../../../../shared/api.service';
 
 @Component({
   selector: 'app-fill-card',
@@ -10,7 +11,22 @@ export class FillCardComponent implements OnInit {
   @Input() lista!:string;
   @Input() id!:string;
   @Input() conteudo!:string;
-  constructor() { }
+  doingDone(id:string,titulo:string, conteudo:string,lista:string){
+    console.log (id,titulo, conteudo,lista)
+    if(lista =="To Do"){
+      this.api.changeCardById(id,titulo, conteudo,"Doing").subscribe((data) => {
+        //this.cards=data;
+        //console.log(this.cards)
+      });
+    }else if(lista =="Doing"){
+      this.api.changeCardById(id,titulo, conteudo,"Done").subscribe((data) => {
+        //this.cards=data;
+        //console.log(this.cards)
+      });;
+    }
+    window.location.reload();
+  }
+  constructor(private api: APIService) { }
 
   ngOnInit(): void {
   }
